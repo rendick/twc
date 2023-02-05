b3bc5ca016da5014c7bb79cff67785aa78fb0b5b
rendick <kostanaddur@gmail.com>
2023-02-05 20:50:05 +0200

    new func. cli menu

--- send.js
+++ send.js
@@ -1,20 +1,32 @@
-const TronWeb = require('tronweb');
-require('dotenv').config()
+const TronWeb = require("tronweb");
+const readline = require("readline");
+const fs = require("fs");
+require("dotenv").config();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 async function sendTRON(privateKey, toAddress, amount) {
-    const tronWeb = new TronWeb({
-        privateKey: process.env.PRIVATE_KEY,
-        fullHost: 'https://api.trongrid.io'
-    });
-  
-    const fromAddress = tronWeb.address.fromPrivateKey(privateKey);
-    const sendAmount = amount * 1000000;
-    const transaction = await tronWeb.transactionBuilder.sendTrx(toAddress, sendAmount, fromAddress);
-    const signedTransaction = await tronWeb.trx.sign(transaction, privateKey);
-    const result = await tronWeb.trx.sendRawTransaction(signedTransaction);
-    console.log(result);
-}
+  const tronWeb = new TronWeb({
+    privateKey: process.env.PRIVATE_KEY,
+    fullHost: "https://api.trongrid.io",
+  });
 
-// Example usage: send 1 TRX from address '41e82D9fBc2cB11be5bE67f6F24c7Bb5E5d5c72C8e'
-// to address 'TKgZGJzNtS1BytBPZXk2A72yJNkx2xgXcR'
-sendTRON(process.env.PRIVATE_KEY, 'TBBshHBLvGZcm4S6tB9PXdGHLrWQe1VhAw', 1);
+  const fromAddress = tronWeb.address.fromPrivateKey(privateKey);
+  const sendAmount = amount * 1000000;
+  const transaction = await tronWeb.transactionBuilder.sendTrx(
+    toAddress,
+    sendAmount,
+    fromAddress
+  );
+  const signedTransaction = await tronWeb.trx.sign(transaction, privateKey);
+  const result = await tronWeb.trx.sendRawTransaction(signedTransaction);
+  console.log(result);
+}
+  const address = rl.question("Receiver`s address: ");
+
+  const trxamount = rl.question("TRX Amount: ");
+
+  sendTRON(process.env.PRIVATE_KEY, address, trxamount);
