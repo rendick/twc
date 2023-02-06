23a6230d61f65f4fb3329293cd8e7c42fd486b09
Kostaintyn Naddur <91757099+naddurkostia@users.noreply.github.com>
2023-02-06 22:18:26 +0200

    auth added

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-const readline = require('readline');
-const fs = require('fs');
+const readline = require("readline");
+const fs = require("fs");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-console.log("Menu:");
+console.log("Menu:\n");
 console.log("1. Account");
 console.log("2. Send TRX");
-console.log("3. Quit");
+console.log("3. Auth");
+console.log("4. Quit");
 
-rl.question("Select an option: ", function(answer) {
+rl.question("\nSelect an option: ", function (answer) {
   switch (answer) {
-    case '1':
-      fs.readFile('account.js', 'utf8', function(err, data) {
+    case "1":
+      fs.readFile("./src/account.js", "utf8", function (err, data) {
         if (err) throw err;
         eval(data);
       });
       break;
-    case '2':
-      fs.readFile('send.js', 'utf8', function(err, data) {
+    case "2":
+      fs.readFile("./src/send.js", "utf8", function (err, data) {
         if (err) throw err;
         eval(data);
       });
       break;
-    case '3':
+    case "3":
+      fs.readFile("./src/auth.js", "utf8", function (err, data) {
+        if (err) throw err;
+        eval(data);
+      });
+      break;
+    case "4":
       console.log("Quitting");
       rl.close();
       break;
