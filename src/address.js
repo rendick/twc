22d7521abaa4cb985dac4a55f2ee4d10292e0841
Kostaintyn Naddur <91757099+rendick@users.noreply.github.com>
2023-02-11 00:02:39 +0200

    `address & amount` func

--- src/address.js
+++ src/address.js
@@ -4,11 +4,37 @@ const fs = require("fs");
 
 const rl = readline.createInterface({ input, output });
 
-rl.question("Receiver`s address: ", (answer) => {
-  fs.appendFile(`./cache/${answer}.txt`, "Hello content!", function (err) {
-    if (err) throw err;
-    console.log("Saved!");
-  });
+// rl.question("Receiver`s address: ", (answer) => {
+//   rl.question("TRX Amount: ", (amount) => {
+//     fs.appendFile(`./cache/address.txt`, `${amount}`, function (err) {
+//       if (err) throw err;
+//       console.log("Saved!");
 
-  rl.close();
+//       fs.appendFile(`./cache/address.txt`, `${answer}`, function (err) {
+//         if (err) throw err;
+//         console.log("Saved!");
+
+//         fs.readFile("./src/send.js", "utf8", function (err, data) {
+//           if (err) throw err;
+//           eval(data);
+
+//           rl.close();
+//         });
+//       });
+//     });
+//   });
+// });
+
+rl.question('Receiver`s address: ', (firstInput) => {
+  rl.question('TRX Amount: ', (secondInput) => {
+    fs.appendFile('./cache/address.txt', `${firstInput}`, function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });
+    fs.appendFile('./cache/amount.txt', `${secondInput}`, function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });
+    rl.close();
+  });
 });
